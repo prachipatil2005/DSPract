9368efae46b25ce943e46380262ab50493defb1d
prachi <guddip515@gmail.com>
2025-02-22 15:51:37 +0530

    updated

--- Pract15Pie_Box_Scatter.R
+++ Pract15Pie_Box_Scatter.R
@@ -1,5 +1,25 @@
-# simple pie chat
-slices <- c(20, 20, 30, 30)
-# slices has the values for the size
-label <- c("India", "US", "Dubai", "France")
-pie(slices, labels = label, main = "Pie chart of Countries")
+#1
+# simple pie chart
+slices <- c(20, 20, 30, 30)  # values for the size
+label <- c("India", "US", "Dubai", "France")  # labels for slices
+# pie chart with colors and title
+pie(slices, labels = label, main = "Pie chart of Countries", col = rainbow(length(slices)))
+
+
+#2 Pie chart with %
+slices<-c(20,20,30,30)
+label<-c("INDIA","US","DUBAI","FRANCE")
+pct<-round(slices/sum(slices)*100)
+label<-paste(label,pct)
+label<-paste(label,"%",sep="")
+pie(slices,labels = label,col = rainbow(length(label)),main="Pie Chat of Countries")
+
+#3
+# Box plot
+par(mar=c(4,4,2,1))
+data(mtcars)
+boxplot(mpg~cyl,data = mtcars,main="cars milage data",xlab="Number of cylinders",ylab = "Miles per gallon")
+
+#4 Scatter diagram
+attach(mtcars)
+plot(wt,mpg,main="ScatterPlot example",xlab="Car weight",ylab="Miles per gallon",pch=19)
