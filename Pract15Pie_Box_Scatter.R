060be9d264af95f5e14e5e928e38f3bbf966bca9
prachi <guddip515@gmail.com>
2025-02-22 13:05:54 +0530

    DSPract15

---
+++ Pract15Pie_Box_Scatter.R
@@ -0,0 +1,5 @@
+# simple pie chat
+slices <- c(20, 20, 30, 30)
+# slices has the values for the size
+label <- c("India", "US", "Dubai", "France")
+pie(slices, labels = label, main = "Pie chart of Countries")
