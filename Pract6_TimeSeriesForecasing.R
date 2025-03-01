f31df4c33d200ea2af773b4d4b257f16c226f097
prachi <guddip515@gmail.com>
2025-03-01 07:21:53 +0530

    pract6

---
+++ Pract6_TimeSeriesForecasing.R
@@ -0,0 +1,12 @@
+#in console
+data("AirPassengers")
+class(AirPassengers)
+start(AirPassengers)
+end(AirPassengers)
+frequency(AirPassengers)
+summary(AirPassengers)
+plot(AirPassengers)
+abline(reg=lm(AirPassengers~time(AirPassengers)))
+cycle(AirPassengers)
+plot(aggregate(AirPassengers,FUN=mean))
+boxplot(AirPassengers~cycle(AirPassengers))
