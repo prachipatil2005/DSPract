5f3712e552d0b36b294293b6514734ccaea7e01c
prachi <guddip515@gmail.com>
2025-03-06 22:31:49 +0530

    update

--- Pract11_OneWayAnova.R
+++ Pract11_OneWayAnova.R
@@ -1,10 +1,9 @@
 #one way anova
 #LOad the dataset
-data1 <- read.csv("C:\Users\prachi\Downloads\One way anova.csv", sep=",", header=TRUE)
+data1 <- read.csv("C:\\Users\\prachi\\Downloads\\One way anova.csv", sep=",", header=TRUE)
 names(data1)
 summary(data1)
 head(data1)
 anv<-aov(satindex~dept,data=data1)
 #Output the anova summary
 summary(anv)
-
