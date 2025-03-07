df3caf1f1cccd897a0af13c249eb515cf6a4079d
prachi <guddip515@gmail.com>
2025-03-07 14:51:48 +0530

    update

--- Pract8_Simple_MultipleLR.R
+++ Pract8_Simple_MultipleLR.R
@@ -17,3 +17,31 @@ new_data<-data.frame(wt=c(3,2.5),hp=c(110,150))
 prediction<-predict(model,newdata=new_data)
 print(prediction)
 plot(prediction,col="red",pch=8,main="Predictions from the MOdel",xlab="Index",ylab="Predicted MPG")
+
+
+##Another Multiple regression
+# Load dataset
+data(mtcars)
+
+# Build multiple linear regression model
+model <- lm(mpg ~ wt + hp, data = mtcars)
+
+# Display summary
+summary(model)
+
+# New data for prediction
+new_data <- data.frame(wt = c(3, 2.5), hp = c(110, 150))
+
+# Predict values
+prediction <- predict(model, newdata = new_data)
+
+# Print predictions
+print(prediction)
+
+# Corrected plot with index
+plot(1:length(prediction), prediction, col = "red", pch = 8, 
+     main = "Predictions from the Model", xlab = "Index", ylab = "Predicted MPG",
+     xlim = c(0, length(prediction) + 1), ylim = range(prediction))
+
+# Add a line connecting points for better visibility
+lines(1:length(prediction), prediction, col = "blue", lty = 2)
